3fe36cc928ce3fd373c5844036aed0680047ae0b
denisphamzero7 <pnhau2001@gmail.com>
2025-08-09 22:27:20 +0700

    update code

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-center pa-5">
+    <h1>Trang About</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="text-center pa-5">
+    <h1>Trang Home</h1>
+
+    <v-btn class="pink white--text"> click me </v-btn>
+    <v-btn depressed color="pink"> click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+    
+    <!-- Test different icon names -->
+    <v-btn><v-icon left>mdi-email</v-icon><span>click here send to email</span></v-btn>
+    <v-btn><v-icon left>mdi-email-outline</v-icon><span>email outline</span></v-btn>
+    <v-btn><v-icon left>mdi-account</v-icon><span>account icon</span></v-btn>
+    <v-btn><v-icon left>mdi-heart</v-icon><span>heart icon</span></v-btn>
+    
+    <!-- Test without left prop -->
+    <v-btn><v-icon>mdi-star</v-icon><span>star icon</span></v-btn>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
