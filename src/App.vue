4ceee7e2c764b1ca47bc7a4d6fdccefd18565105
van-dat <dangvandat1d@gmail.com>
2025-08-10 07:06:00 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
-  <v-app>
-     <v-app-bar app>
-      <v-btn to="/" text>Home</v-btn>
-      <v-btn to="/about" text>About</v-btn>
-    </v-app-bar>
-
+  <v-app class="grey lighten-4">
+    <Navbar/>
     <v-main>
-
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from './views/Navbar.vue';
+
 
 
 export default {
   name: 'App',
 
   components: {
-   
+   Navbar
   },
 
   data: () => ({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,9 @@
   <div class="text-center pa-5">
     <h1>Trang Home</h1>
 
-    <v-btn class="pink white--text"> click me </v-btn>
-    <v-btn depressed color="pink"> click me</v-btn>
-    <v-btn text color="pink">click me</v-btn>
+    <v-btn class="hidden-md-and-down pink white--text "> click me111qqq </v-btn>
+    <v-btn depressed color="pink" class="hidden-sm-and-up"> click me11d1</v-btn>
+    <v-btn text color="pink" class="hidden-sm-only">click me</v-btn>
     
     <!-- Test different icon names -->
     <v-btn><v-icon left>mdi-email</v-icon><span>click here send to email</span></v-btn>
